a547c7148f85f41560b0b477a12bc8467a0d0b04
Xhi3r <aroundthegreat@gmail.com>
2024-06-25 10:03:50 +0700

    -

--- examples/02_chassis/05_sub_position.py
+++ examples/02_chassis/05_sub_position.py
@@ -23,15 +23,42 @@ def sub_position_handler(position_info):
     print("chassis position: x:{0}, y:{1}, z:{2}".format(x, y, z))
 
 
+
+x_val = 0.5
+y_val = 0.6
+z_val = 90
+
 if __name__ == '__main__':
     ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="sta")
+    ep_robot.initialize(conn_type="ap")
 
     ep_chassis = ep_robot.chassis
 
     # 订阅底盘位置信息
-    ep_chassis.sub_position(freq=10, callback=sub_position_handler)
-    ep_chassis.move(x=1.0, y=1.0, z=90).wait_for_completed()
+    ep_chassis.sub_position(freq=1, callback=sub_position_handler)
+    #ep_chassis.move(x=1.0, y=1.0, z=0).wait_for_completed()
+    
+    ep_chassis.move(x=x_val, y=0, z=0, xy_speed=0.7).wait_for_completed()
+     # 左转 90度
+    
+    # 左移 0.6米
+    ep_chassis.move(x=0, y=-y_val, z=0, xy_speed=0.7).wait_for_completed()
+    
+     # 左转 90度
+    
+    # 后退 0.5米
+    ep_chassis.move(x=-x_val, y=0, z=0, xy_speed=0.7).wait_for_completed()
+   
+     # 左转 90度
+    
+    # 右移 0.6米
+    ep_chassis.move(x=0, y=y_val, z=0, xy_speed=0.7).wait_for_completed()
+
+   
+
+    # 右转 90度
+    #ep_chassis.move(x=0, y=0, z=-z_val, z_speed=45).wait_for_completed()
+
     ep_chassis.unsub_position()
 
     ep_robot.close()
